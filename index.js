09d0fc283eb4efee5fd4f3a8ab7c095809915ddd
Duncan Beaton <dunckr@gmail.com>
2014-11-02 17:52:04 +0000

    add infinite scrolling more users

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-var Nightmare = require('nightmare');
-
-module.exports = pymk = function(options) {
-    if (options.email === undefined) return 'require email';
-    if (options.password === undefined) return 'require password';
-
-    new Nightmare()
-        .use(login(options.email, options.password))
-        .screenshot('test.png')
-        .run(function(err, nightmare) {
-            console.log(err);
-        });
-};
+var Nightmare = require('nightmare'),
+    $ = require('cheerio');
 
 var login = exports.login = function(email, password) {
     return function(nightmare) {
@@ -19,6 +8,40 @@ var login = exports.login = function(email, password) {
             .type('#session_key-login', email)
             .type('#session_password-login', password)
             .click('#btn-primary')
-            .wait()
+            .wait();
     };
+};
+
+var list = exports.list = function() {
+    return function(nightmare) {
+        nightmare
+            .goto('https://www.linkedin.com/people/pymk')
+            .wait();
+    };
+};
+
+var getNext = exports.getNext = function() {
+    return function(nightmare) {
+        nightmare
+            .evaluate(function() {
+                return $(window).scrollTop($(document).height());
+            })
+            .wait();
+    };
+};
+
+module.exports = pymk = function(options) {
+    if (options.email === undefined) return 'require email';
+    if (options.password === undefined) return 'require password';
+
+    new Nightmare()
+        .use(login(options.email, options.password))
+        .use(list())
+        .use(getNext())
+        .use(getNext())
+        .use(getNext())
+        .screenshot('test.png')
+        .run(function(err, nightmare) {
+            console.log(err);
+        });
 };
