29050ffcf5ac7f63f2b6bf55d46313a7cf505e62
Duncan Beaton <dunckr@gmail.com>
2014-11-10 14:17:04 +0000

    add test example with timebetweenvisits and duration

--- test/index.js
+++ test/index.js
@@ -2,7 +2,9 @@ var pymk = require('../index');
 
 var options = {
     email: 'yo@myemail.com',
-    password: 'supersecret'
+    password: 'supersecret',
+    maxTimesVisit: 3,
+    timeBetweenVists: 60 * 60 * 24 * 10
 };
 
 pymk(options);
