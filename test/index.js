388ff853ad9ba260e830bc8469a5281713a9a6a5
Duncan Beaton <dunckr@gmail.com>
2014-11-14 14:11:11 +0000

    fix start re-structuring with tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,43 @@
-var pymk = require('../index');
+var Pymk = require('../index');
 
-var options = {
-    email: 'yo@myemail.com',
-    password: 'supersecret',
-    maxTimesVisit: 3,
-    timeBetweenVists: 60 * 60 * 24 * 10
-};
+describe('pymk', function() {
 
-pymk(options);
+    var pymk;
+
+    beforeEach(function() {
+        pymk = new Pymk({
+            email: 'test@email.com',
+            password: 'topSecret'
+        });
+    });
+
+    it('should throw error without options', function() {
+        (function() {
+            return new Pymk()
+        }).should.throw('require login options');
+    });
+
+    it('should throw error without email options', function() {
+        (function() {
+            return new Pymk({
+                password: '1pass'
+            })
+        }).should.throw('require email');
+    });
+
+    it('should throw error without password options', function() {
+        (function() {
+            return new Pymk({
+                email: 'test@email.com'
+            })
+        }).should.throw('require password');
+    });
+
+
+    it('should parse the url for the id', function() {
+        var url = 'https://www.linkedin.com/profile/view?id=012345678&amp;authType=name&amp;authToken=8o5N&amp;trk=connect_hub_pymk_profile_photo';
+        (pymk.parseUrl(url)).should.equal('012345678');
+    });
+
+
+});
